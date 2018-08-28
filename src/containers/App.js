fa6145315a0108465e421efbec326559746947e2
vishal <vishal.joshi.021@gmail.com>
2018-08-27 20:30:45 -0400

    higher order component returning statefull as well as stateless components

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,7 +2,8 @@ import React, { PureComponent } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
-import WithClass from '../hoc/WithClass';
+import Aux from '../hoc/Auxilliary';
+import withClass from '../hoc/withClass';
 
 class App extends PureComponent {
   constructor(props) {
@@ -77,8 +78,8 @@ class App extends PureComponent {
     }
 
     return (
-        <WithClass classes={classes.App}>
-        <button onClick={() => {this.setState({showPersons: true})}}>Show Persons</button>
+        <Aux>
+          <button onClick={() => {this.setState({showPersons: true})}}>Show Persons</button>
           <Cockpit
           appTitle={this.props.title}
           showPersons={this.state.showPersons}
@@ -86,9 +87,9 @@ class App extends PureComponent {
           clicked={this.togglePersonsHandler}
           />
           {persons}
-        </WithClass>
+        </Aux>
     );
   }
 }
 
-export default App;
+export default withClass(App, classes.App);
